e6d71ae97c7d7db4e816cc6a22abaec462b756b1
Tigran Manasyan <manasyan_tigran@mail.ru>
2020-02-08 22:35:02 +0700

    algorithm part was realised

--- src/TreeTrainer.kt
+++ src/TreeTrainer.kt
@@ -1,6 +1,5 @@
 import models.DataEntity
 import models.TreeNode
-import kotlin.math.ln
 import kotlin.math.log
 
 class TreeTrainer() {
@@ -12,6 +11,8 @@ class TreeTrainer() {
         initAttributes(trainingEntities)
     }
 
+    private data class AttributeContext(val attributeInfo : Double, val splitInfos : Double)
+
     private fun initAttributes(trainingEntities: List<DataEntity>) {
         trainingEntities.forEach {
             for (i in it.attributes.indices) {
@@ -21,26 +22,96 @@ class TreeTrainer() {
         }
     }
 
-    private fun getSplitInfo(attributeIndex: Int, trainingEntities: List<DataEntity>){
+    private fun getSplitInfo(trainingEntities: List<DataEntity>, attributeIndex: Int) : Double{
+        val frequency = getFrequency(trainingEntities, attributeIndex)
+        return calculateEntropy(trainingEntities, frequency)
+    }
+
+    private fun getInfo(trainingEntities: List<DataEntity>) : Double{
+        val frequency = getFrequency(trainingEntities, trainingEntities.size - 1)
+        return -1 * calculateEntropy(trainingEntities, frequency)
+    }
+
+    private fun getAttributeInfo(trainingEntities: List<DataEntity>, attributeIndex: Int) : Double{
         val entitiesCount = trainingEntities.size
-        val frequency : MutableMap<String, Int> = HashMap()
-
-        trainingEntities.forEach{ entity ->
-            entity.attributes.forEach{
-                frequency[it] = frequency.getOrDefault(it, 0) + 1
-            }
-        }
-
-        frequency.values.fold(0.0){ acc, freq ->
-            acc + (freq/entitiesCount) * ln((freq/entitiesCount).toDouble())
+        val classFrequency = getFrequency(trainingEntities, entitiesCount - 1)
+        return classFrequency.values.fold(0.0){ acc, freq ->
+            acc + (freq/entitiesCount) * getInfo(trainingEntities)
         }
     }
 
+    private fun getFrequency(trainingEntities: List<DataEntity>, attributeIndex: Int): Map<String, Int> {
+        val frequency : MutableMap<String, Int> = HashMap()
 
-    fun train(head: TreeNode?, trainingEntities: List<DataEntity>) {
-        for(i in attributesMap.keys){
-
+        trainingEntities.forEach{ entity ->
+            val attributeValue = entity.attributes[attributeIndex]
+            frequency[attributeValue] = frequency.getOrDefault(attributeValue, 0) + 1
         }
+
+        return frequency
+    }
+
+    private fun calculateEntropy(trainingEntities: List<DataEntity>, frequency : Map<String, Int>) : Double{
+        val entitiesCount = trainingEntities.size
+        return frequency.values.fold(0.0){ acc, freq ->
+            acc + (freq/entitiesCount) * log((freq/entitiesCount).toDouble(), 2.0)
+        }
+    }
+
+    private fun divideByAttribute(trainingEntities: List<DataEntity>, attributeIndex: Int) : Map<String, MutableList<DataEntity>>{
+        val groups : MutableMap<String, MutableList<DataEntity>> = HashMap()
+        attributesMap[attributeIndex]?.forEach{
+            groups[it] = ArrayList()
+        }
+
+        trainingEntities.forEach{
+            val attributeValue = it.attributes[attributeIndex]
+            groups[attributeValue]?.add(it)
+        }
+
+        return groups
+    }
+
+    private fun makeLeaf(treeNode: TreeNode, attributeValue : String, trainingEntities: List<DataEntity>){
+        val frequency = getFrequency(trainingEntities, trainingEntities.size - 1)
+        val leaf = TreeNode()
+        leaf.label = frequency.maxBy { it.value }?.key
+        treeNode.addChild(attributeValue, leaf)
+    }
+
+    fun train(treeNode: TreeNode?, trainingEntities: List<DataEntity>) : Boolean {
+        treeNode ?: return false
+
+        if(trainingEntities.isEmpty()){
+            return false
+        }
+
+        val info = getInfo(trainingEntities)
+        val attributeContexts = ArrayList<AttributeContext>()
+
+        for(i in attributesMap.keys){
+            val attrInfo = getAttributeInfo(trainingEntities, i)
+            val splitInfo =  getSplitInfo(trainingEntities, i)
+            attributeContexts.add(AttributeContext(attrInfo, splitInfo))
+        }
+
+        var max = 0.0
+        val indexToDivide= attributeContexts.foldIndexed(0){ index, acc, context ->
+            val gainRatio = (info - context.attributeInfo) / context.splitInfos
+            if(gainRatio > max) {
+                max = gainRatio
+                index
+            } else acc
+        }
+
+        divideByAttribute(trainingEntities, indexToDivide).forEach{
+            val child = TreeNode()
+            treeNode.addChild(it.key, child)
+            if(!train(child, it.value))
+                makeLeaf(treeNode, it.key, trainingEntities)
+        }
+
+        return true
     }
 }
 
@@ -49,5 +120,5 @@ fun main(){
     val entities = parseCsv("test.csv")
     val trainer = TreeTrainer(entities)
 
-    trainer.train(null, entities)
+    trainer.train(TreeNode(), entities)
 }
--- src/models/TreeNode.kt
+++ src/models/TreeNode.kt
@@ -1,3 +1,6 @@
 package models
 
-data class TreeNode(var children : MutableMap<String, TreeNode>) {}
+data class TreeNode(var children : MutableMap<String, TreeNode> = HashMap(), var label: String? = null) {
+    fun addChild(childIn : String, childNode: TreeNode) = children.put(childIn, childNode)
+
+}
